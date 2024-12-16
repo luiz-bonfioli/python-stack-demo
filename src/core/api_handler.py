27a2d8aad11f19dd3a12543ddf0e36fab3b2a822
Luiz Bonfioli <luiz.bonfioli@syngenta.com>
2024-12-16 12:13:46 -0300

    Update api_handler.py

--- src/core/api_handler.py
+++ src/core/api_handler.py
@@ -1,9 +1,5 @@
-# from mangum import Mangum
-
 from src.application.controller import app
 from src.core.common.config import WEB_SERVER_HOST, WEB_SERVER_PORT
-#
-# api_handler = Mangum(app)
 
 # To run locally
 if __name__ == '__main__':
