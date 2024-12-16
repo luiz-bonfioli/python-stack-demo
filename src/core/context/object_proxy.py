aeba7f86b2cf0497c4cab0c47608ffb4fe16758b
Luiz Bonfioli <luiz.bonfioli@syngenta.com>
2024-12-16 12:13:15 -0300

    Update object_proxy.py

--- src/core/context/object_proxy.py
+++ src/core/context/object_proxy.py
@@ -1,4 +1,14 @@
 class ObjectProxy:
+    """
+    ObjectProxy is a lazy initialization proxy that is used to instantiate an object only when
+    it is actually needed. The proxy holds a reference to an object generator (usually a
+    callable that creates the object), and it will only create the object when one of its
+    attributes or methods is accessed.
+
+    This class is useful when you want to delay the creation of an object to improve
+    performance, or if the object creation is expensive and you want to create it only
+    when required.
+    """
 
     def __init__(self, object_generator):
         self.object_generator = object_generator
