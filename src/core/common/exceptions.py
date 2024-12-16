fe37d24f88a1e5b05849d14c18f770e9630486d9
Luiz Bonfioli <luiz.bonfioli@syngenta.com>
2024-12-16 12:21:27 -0300

    Comments

--- src/core/common/exceptions.py
+++ src/core/common/exceptions.py
@@ -1,4 +1,11 @@
 class KafkaPublishException(Exception):
+    """
+    Custom exception raised when an error occurs while publishing a message to a Kafka topic.
+
+    This exception is designed to provide detailed information about the failure of
+    a message publication, including the topic, the message that was attempted to be
+    published, and the associated error message from Kafka.
+    """
     def __init__(self, topic: str, published_message: str, error_message: str) -> None:
         message = f'Error to send message {published_message} to topic {topic}, ERROR - {error_message}'
         self.topic = topic
--- src/core/common/uuid_utils.py
+++ src/core/common/uuid_utils.py
@@ -2,4 +2,18 @@ from uuid import UUID
 
 
 def safe_uuid_to_str(value: UUID):
+    """
+    Safely converts a UUID object to a string representation.
+
+    This function takes a UUID object and returns its string representation if the UUID is
+    valid (i.e., not None). If the input value is None, the function returns None instead
+    of raising an error. This ensures that the function handles cases where the UUID is
+    missing or not provided without throwing an exception.
+
+    Args:
+        value (UUID): A UUID object to be converted to a string.
+
+    Returns:
+        Optional[str]: The string representation of the UUID if it is not None, otherwise None.
+    """
     return str(value) if value else None
